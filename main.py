fa096e6dc2780934f0da71754d89d5a9aa2e6011
vodaiphuoc <phuocvodn98@gmail.com>
2026-06-12 10:15:08 +0700

    feat(diarization): add custom export method

--- main.py
+++
@@ -1,9 +0,0 @@
-from SDP import Pipeline
-
-SAMPLING_RATE = 16000
-
-if __name__ == "__main__":
-    p =  Pipeline()
-    audio_file = "examples/part1/bacsidatnhkhoavitadoc_1.wav"
-    p.forward(audio_file, sampling_rate=SAMPLING_RATE)
-    
