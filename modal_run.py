6e90b77647f8c711efb993f7cabca58bd18fd848
vodaiphuoc <phuocvodn98@gmail.com>
2026-06-16 21:38:17 +0700

    clean up

--- modal_run.py
+++ modal_run.py
@@ -1,13 +1,14 @@
 # Script for runing pipeline on modal.com
 
-import modal
-import os
 import json
+import os
+
+import modal
 
 dockerfile_image = modal.Image.from_dockerfile(
-    "./Dockerfile",
+    "./docker/Dockerfile",
     context_dir=os.path.dirname(__file__),
-    ignore="./.dockerignore"
+    ignore="./.dockerignore",
 ).add_local_dir(
     local_path="./data/part1/",
     remote_path="/data/part1/",
@@ -15,41 +16,41 @@ dockerfile_image = modal.Image.from_dockerfile(
 
 app = modal.App("pipeline-inference-v2")
 
-@app.function(
-        image=dockerfile_image,
-        gpu = "T4",
-        timeout = 60*20
-)
+
+@app.function(image=dockerfile_image, gpu="T4", timeout=60 * 20)
 def run():
-    import subprocess
     import os
-    subprocess.run(["pip", "install", "typing_extensions>=4.14.0", "--ignore-installed"], check=True)
+    import subprocess
+
+    subprocess.run(
+        ["pip", "install", "typing_extensions>=4.14.0", "--ignore-installed"],
+        check=True,
+    )
 
     os.environ["LOG_LEVEL"] = "INFO"
-    from SDP import Pipeline
     import glob
+
+    from SDP.transformers.pipeline import Pipeline
 
     SAMPLING_RATE = 16000
 
     assert os.path.isdir("/data/part1")
     audio_list = glob.glob("/data/part1/*.wav")
 
-    p =  Pipeline()
-    
+    p = Pipeline()
+
     batch_results = []
     for audio_file in audio_list:
         assert os.path.isfile(audio_file)
         outputs = p.forward(audio_file, sampling_rate=SAMPLING_RATE)
-        batch_results.append({
-            "audio_file": audio_file,
-            "results": outputs
-        })
+        batch_results.append({"audio_file": audio_file, "results": outputs})
 
     return batch_results
+
 
 @app.local_entrypoint()
 def main():
     r = run.remote()
-    
-    with open("data_results/part1/results.json","w", encoding="utf-8") as fp:
+
+    with open("data_results/part1/results.json", "w", encoding="utf-8") as fp:
         json.dump(r, fp, ensure_ascii=False, indent=4)
