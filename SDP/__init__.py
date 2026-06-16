74fa62de9a873030b58c112cb1273a0925a676c7
vodaiphuoc <phuocvodn98@gmail.com>
2026-06-16 21:45:24 +0700

    temporary use onnx module only

--- SDP/__init__.py
+++ SDP/__init__.py
@@ -1,4 +1,21 @@
-from .pipeline import Pipeline
+# from .transformers.pipeline import Pipeline
+# from .utils import decode_audio
+
+# __all__ = ["Pipeline", "decode_audio"]
+
+
+from .onnx.diarization.utils import (
+    load_encoder_modules_config,
+    load_preprocessor_config,
+    load_sortformer_modules_config,
+)
+from .onnx.streaming_service import StreamingDiarizerOnnxService
 from .utils import decode_audio
 
-__all__ = ["Pipeline","decode_audio"]
+__all__ = [
+    "StreamingDiarizerOnnxService",
+    "load_encoder_modules_config",
+    "load_preprocessor_config",
+    "load_sortformer_modules_config",
+    "decode_audio",
+]
